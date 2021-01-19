0aeb93eacb5a5b0245ccd4128b55637e74f6cb64
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-01-19 12:12:46 -0300

    finalizando bdd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Cliente = require('../models/cliente');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let cpf = req.query.cpf
+  var cliente = new Cliente()
+  cliente.cpf = cpf
+  let cpfValidado = cliente.validarCPF()
+  res.render('index', { title: 'Suppi Store', cpfValidado:cpfValidado, cpf:cpf });
 });
 
 module.exports = router;
